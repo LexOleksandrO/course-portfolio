64765a37e10ad44af28e7e322b8827b5cff577f2
LexOleksandro <lex.learns.coding@gmail.com>
2022-04-19 23:52:53 +0300

    image insertion task completed

--- index.js
+++ index.js
@@ -4,3 +4,21 @@
 //(будьякий елемент).append - додає до елемента
 //document.createElement("div") - повертає новостворений елемент типу який переданий в якості парамету
 
+
+///// HOMEWORK 1 /////
+
+//1// Вставити зображення в блок "mainTable__image":
+//img URl = https://academy.stfalcon.com/images/gallery/all/g_pict7.jpg
+
+function imageInsertion () {
+  const imgParentContainer = document.body.children[0].children[0].children[1].children[0];
+  const imgContainer = document.createElement('img');
+  imgParentContainer.innerHTML = '';
+
+  const imgTagAttrs = imgContainer.setAttribute('src', 'https://academy.stfalcon.com/images/gallery/all/g_pict7.jpg');
+  imgParentContainer.append(imgContainer);
+}
+
+setTimeout(imageInsertion, 4000);
+
+//2// 
